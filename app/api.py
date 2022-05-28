444efcfb8005218c09e7d3159b64f645c11917a4
wences-sanchez <wenceslaosanchezpino@gmail.com>
2022-05-28 12:04:00 +0200

    Add /calc/multiply/<op_1>/<op_2> endpoint to api.py
    Endpoint with the Calculator.multiply feature

--- app/api.py
+++ app/api.py
@@ -31,3 +31,12 @@ def substract(op_1, op_2):
         return ("{}".format(CALCULATOR.substract(num_1, num_2)), http.client.OK, HEADERS)
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
+
+
+@api_application.route("/calc/multiply/<op_1>/<op_2>", methods=["GET"])
+def multiply(op_1, op_2):
+    try:
+        num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
+        return "{}".format(CALCULATOR.multiply(num_1, num_2)), http.client.OK, HEADERS
+    except TypeError as e:
+        return str(e), http.client.BAD_REQUEST, HEADERS
