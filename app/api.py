918389415f639fa17ac86d23fff35f3121d17fbb
wences-sanchez <wenceslaosanchezpino@gmail.com>
2022-05-28 12:28:27 +0200

    Add /calc/power/<op_1>/<op_2> endpoint
    Endpoint added with the feature of Calculator.power()

--- app/api.py
+++ app/api.py
@@ -50,3 +50,13 @@ def divide(op_1, op_2):
     except TypeError as e:
         return str(e), http.client.BAD_REQUEST, HEADERS
 
+
+@api_application.route("/calc/power/<op_1>/<op_2>", methods=["GET"])
+def power(op_1, op_2):
+    try:
+        num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
+        return "{}".format(CALCULATOR.power(num_1, num_2)), http.client.OK, HEADERS
+    except TypeError as e:
+        return str(e), http.client.BAD_REQUEST, HEADERS
+
+
