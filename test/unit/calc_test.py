8252cbcb12a1573e342849aa32e6a97aa1c08f43
wences-sanchez <wenceslaosanchezpino@gmail.com>
2022-05-30 09:25:17 +0200

    Complete positive tests with success

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -24,15 +24,6 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(1, self.calc.divide(2, 2))
         self.assertEqual(1.5, self.calc.divide(3, 2))
 
-    def test_add_method_fails_with_nan_parameter(self):
-        self.assertRaises(TypeError, self.calc.add, "2", 2)
-        self.assertRaises(TypeError, self.calc.add, 2, "2")
-        self.assertRaises(TypeError, self.calc.add, "2", "2")
-        self.assertRaises(TypeError, self.calc.add, None, 2)
-        self.assertRaises(TypeError, self.calc.add, 2, None)
-        self.assertRaises(TypeError, self.calc.add, object(), 2)
-        self.assertRaises(TypeError, self.calc.add, 2, object())
-
     def test_divide_method_fails_with_nan_parameter(self):
         self.assertRaises(TypeError, self.calc.divide, "2", 2)
         self.assertRaises(TypeError, self.calc.divide, 2, "2")
@@ -50,6 +41,68 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(0, self.calc.multiply(1, 0))
         self.assertEqual(0, self.calc.multiply(-1, 0))
         self.assertEqual(-2, self.calc.multiply(-1, 2))
+
+    """
+    TEST POSITIVOS
+    """
+
+    # Testear que la resta calcula bien según sus parámetros
+    def test_substract_method_returns_correct_result(self):
+        self.assertEqual(0, self.calc.substract(2, 2))
+        self.assertEqual(0, self.calc.substract(0, 0))
+        self.assertEqual(5, self.calc.substract(10, 5))
+        self.assertEqual(-5, self.calc.substract(5, 10))
+        self.assertEqual(15, self.calc.substract(10, -5))
+        self.assertEqual(-30, self.calc.substract(-20, 10))
+        self.assertEqual(0, self.calc.substract(-10, -10))
+
+    # Testear que la división calcula bien según sus parámetros
+    def test_divide_method_returns_other_correct_result(self):
+        self.assertEqual(1, self.calc.divide(5, 5))
+        self.assertEqual(0.5, self.calc.divide(5, 10))
+        self.assertEqual(1.5, self.calc.divide(7.5, 5))
+
+    # Testear que la multiplicación calcula bien según sus parámetros
+    @patch('app.util.validate_permissions', side_effect=mocked_validation, create=True)
+    def test_multiply_method_returns_other_correct_result(self, _validate_permissions):
+        self.assertEqual(0, self.calc.multiply(4, 0))
+        self.assertEqual(0, self.calc.multiply(0, 4))
+        self.assertEqual(80, self.calc.multiply(20, 4))
+        self.assertEqual(-50, self.calc.multiply(-5, 10))
+        self.assertEqual(-1000, self.calc.multiply(10, -100))
+        self.assertEqual(25, self.calc.multiply(-5, -5))
+
+    # Testear que la potencia calcula bien según sus parámetros
+    def test_power_method_returns_correct_result(self):
+        self.assertEqual(1, self.calc.power(500, 0))
+        self.assertEqual(20, self.calc.power(20, 1))
+        self.assertEqual(16, self.calc.power(2, 4))
+        self.assertEqual(100, self.calc.power(10, 2))
+        self.assertEqual(1024, self.calc.power(2, 10))
+
+    # Testear que la raíz calcula bien según su parámetro
+    def test_squared_root_returns_correct_result(self):
+        self.assertEqual(4, self.calc.squared_root(16))
+        self.assertEqual(1, self.calc.squared_root(1))
+
+    # Testear que el logaritmo de base 10 calcula bien según su parámetro
+    def test_log_10_returns_correct_result(self):
+        self.assertEqual(2, self.calc.log_10(100))
+        self.assertEqual(0, self.calc.log_10(1))
+        self.assertEqual(1, self.calc.log_10(10))
+
+    """
+    TESTS NEGATIVOS (comprobamos que lanza excepción)
+    """
+
+    # Testear que la resta lanza excepción en caso de error en sus parámetros
+    # Testear que la división lanza excepción en caso de error en sus parámetros
+    # Testear que la multiplicación lanza excepción en caso de error en sus parámetros
+    # Testear que la potencia lanza excepción en caso de error en su parámetro
+    # Testear que la potencia lanza excepción en caso de error en sus parámetros
+    # Testear que la raíz lanza excepción en caso de error en su parámetro
+    # Testear que el logaritmo de base 10 lanza excepción en caso de error en su parámetro
+
 
 
 if __name__ == "__main__":  # pragma: no cover
